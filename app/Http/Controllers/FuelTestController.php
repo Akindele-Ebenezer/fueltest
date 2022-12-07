763ebc99e4ac605c7b23a6e323a67a4ca4b0b3dd
Akindele Ebenezer <tobi.akindele@gmail.com>
2022-12-07 10:02:02 +0100

    Optimizing App

--- app/Http/Controllers/FuelTestController.php
+++ app/Http/Controllers/FuelTestController.php
@@ -772,33 +772,33 @@ class FuelTestController extends Controller
 
             $ViewData = [...$Config, ...$ViewData]; 
             
-            // if(isset($_GET['export'])) {
-            //     $ClearDynamicExport = DynamicExport::query()->delete();
+            if(isset($_GET['export'])) {
+                $ClearDynamicExport = DynamicExport::query()->delete();
 
-            //     $ExportRecords = $all_records_absolute; 
-            //     foreach($ExportRecords as $ExportRecord) {
-            //         $create_record = DynamicExport::create([
-            //         'SampleNo' => $ExportRecord->SampleNo,
-            //         'SampleCollectionDate' => $ExportRecord->SampleCollectionDate,
-            //         'TruckPlateNo' => $ExportRecord->TruckPlateNo,
-            //         'TankNo' => $ExportRecord->TankNo,
-            //         'AppearanceResult' => $ExportRecord->AppearanceResult,
-            //         'Color' => $ExportRecord->Color,
-            //         'Density' => $ExportRecord->Density,
-            //         'FlashPoint' => $ExportRecord->FlashPoint,
-            //         'Temp' => $ExportRecord->Temp,
-            //         'WaterSediment' => $ExportRecord->WaterSediment,
-            //         'Cleanliness' => $ExportRecord->Cleanliness,
-            //         'DateOfTest' => $ExportRecord->DateOfTest,
-            //         'uid' => $ExportRecord->uid,
-            //         'MadeBy' => $ExportRecord->MadeBy,
-            //         'DeliveredTo' => $ExportRecord->DeliveredTo,
-            //         'Remarks' => $ExportRecord->Remarks,
-            //         'VendorName' => $ExportRecord->VendorName,
-            //         'VendorNo' => $ExportRecord->VendorNo,
-            //         'ApprovalForUse' => $ExportRecord->ApprovalForUse,
-            //         ]);   
-            //     }
+                $ExportRecords = $all_records_absolute; 
+                foreach($ExportRecords as $ExportRecord) {
+                    $create_record = DynamicExport::create([
+                    'SampleNo' => $ExportRecord->SampleNo,
+                    'SampleCollectionDate' => $ExportRecord->SampleCollectionDate,
+                    'TruckPlateNo' => $ExportRecord->TruckPlateNo,
+                    'TankNo' => $ExportRecord->TankNo,
+                    'AppearanceResult' => $ExportRecord->AppearanceResult,
+                    'Color' => $ExportRecord->Color,
+                    'Density' => $ExportRecord->Density,
+                    'FlashPoint' => $ExportRecord->FlashPoint,
+                    'Temp' => $ExportRecord->Temp,
+                    'WaterSediment' => $ExportRecord->WaterSediment,
+                    'Cleanliness' => $ExportRecord->Cleanliness,
+                    'DateOfTest' => $ExportRecord->DateOfTest,
+                    'uid' => $ExportRecord->uid,
+                    'MadeBy' => $ExportRecord->MadeBy,
+                    'DeliveredTo' => $ExportRecord->DeliveredTo,
+                    'Remarks' => $ExportRecord->Remarks,
+                    'VendorName' => $ExportRecord->VendorName,
+                    'VendorNo' => $ExportRecord->VendorNo,
+                    'ApprovalForUse' => $ExportRecord->ApprovalForUse,
+                    ]);   
+                }
             }
 
             if (isset($_GET['Search'])) {
