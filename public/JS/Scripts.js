1163cb224271f008413a0aa99995265b24954fb4
Akindele Ebenezer <tobi.akindele@gmail.com>
2023-01-31 23:25:42 +0100

    Adding subsections to Navigation

--- public/JS/Scripts.js
+++ public/JS/Scripts.js
@@ -64,3 +64,11 @@ VendorNoInputBox.addEventListener('input', (VendorNo) => {
         } 
     }        
 }); 
+
+let Arrows = document.querySelectorAll('section.fuel-test .fuel-test-left-nav ul span img');
+
+Arrows.forEach(Arrow => {
+    Arrow.addEventListener('click', () => {
+        Arrow.parentElement.nextElementSibling.classList.toggle('show'); 
+    });
+});
